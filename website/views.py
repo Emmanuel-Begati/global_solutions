ad3719d5c23020b46c534cc66e6a977b60c0d9b5
Emmanue-Begati <e.yakubu@alustudent.com>
2025-02-20 14:37:22 +0200

    changing some stuff;

--- website/views.py
+++ website/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'website/index.html')
+
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def services(request):
+    return render(request, 'website/services.html')
+
+def pricing(request):
+    return render(request, 'website/pricing.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
+
+def team(request):
+    return render(request, 'website/team.html')
+
+def base(request):
+    return render(request, 'website/base.html')
