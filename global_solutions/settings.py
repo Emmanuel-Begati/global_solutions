8a541b738569ab04f3623bb1631261a4124d18a5
Emmanuel-Begati <e.yakubu@alustudent.com>
2025-04-06 14:01:59 +0200

    done

--- global_solutions/settings.py
+++ global_solutions/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-wf13ca#_5%p(5b=6jnqu+6w#@^n_#0p7hc0=6+tap5ioit3jnv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['global-solutions-rbia.onrender.com' ]
-
+ALLOWED_HOSTS = ['global-solutions-rbia.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # This is the new line you need to add
+
+# Add this to your settings.py file
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
